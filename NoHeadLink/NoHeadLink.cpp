051e1b83c1bf049a97d2fa584ccaf314f38a647f
Tiu.G <171380979@qq.com>
2012-08-15 16:43:32 +0800

    08.15 16:45

--- NoHeadLink/NoHeadLink.cpp
+++ NoHeadLink/NoHeadLink.cpp
@@ -1,3 +1,7 @@
+/*****************************************************/
+/*假设有一个没有头指针的单链表。一个指针指向此单链表**/
+/*中间的一个节点，请将该节点从单链表中删除************/
+/*****************************************************/
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
@@ -12,19 +16,26 @@ void DeleteRandomNode(Node * pCurrent);
 
 int main()
 {
-	Node first,second,thrid,fourth;
+	Node *first = (Node *)malloc(sizeof(Node));
+	Node *second = (Node *)malloc(sizeof(Node));
+	Node *thrid = (Node *)malloc(sizeof(Node));
+	Node *fourth = (Node *)malloc(sizeof(Node));
+
 /*Init*/
-	first.value = 'A';
-	first.next = &second;
-	second.value = 'B';
-	second.next = &thrid;
-	thrid.value = 'C';
-	thrid.next = &fourth;
-	fourth.value = 'D';
-	fourth.next = NULL;
-	printf("%c->%c->%c->%c\n",first.value,second.value,thrid.value,fourth.value);
-	DeleteRandomNode(&second);
-	printf("%c->%c->%c\n",first.value,second.value,fourth.value);
+	first->value = 'A';
+	first->next = second;
+	second->value = 'B';
+	second->next = thrid;
+	thrid->value = 'C';
+	thrid->next = fourth;
+	fourth->value = 'D';
+	fourth->next = NULL;
+
+	printf("%c->%c->%c->%c\n",first->value,second->value,thrid->value,fourth->value);
+
+	DeleteRandomNode(second);
+
+	printf("%c->%c->%c\n",first->value,second->value,fourth->value);
 
 	return 0;
 }
